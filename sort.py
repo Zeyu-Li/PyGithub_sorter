efc226c99a583fa77e13b914a4443d55983a8f9f
Andrew Li <34993025+Zeyu-Li@users.noreply.github.com>
2020-01-04 11:07:50 -0600

    Update sort.py

--- sort.py
+++ sort.py
@@ -17,8 +17,9 @@ def main():
 
     # sorts public repos alphabetically and prints them
     reponames = sorted(repo.name for repo in repos)
+    reponames = sorted(reponames, key=str.casefold)
 
-    print(f"You have {len(reponames)} under your user.")
+    print(f"You have {len(reponames)} under your {your_name}.")
     print("The list of repositories is the following:")
     for name in reponames:
         print(name)
